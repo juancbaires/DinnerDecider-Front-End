43fd28362d8505c7d38748b378b09a3454e4cf84
Juan Baires <juancbaires@gmail.com>
2018-11-11 22:11:53 -0500

    implemented firebase as my database in the config file and have geolocation in state.

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -1,28 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { config } from './components/Config/Config'
+import firedatabase from 'firebase/app';
+import 'firebase/database';
+// import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+import firebase from "firebase"
+import FavoriteFoods from './components/FavoriteFoods/FavoriteFoods';
 
-class App extends Component {
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      latitude: '',
+      longitude: '',
+      favorite_foods: []
+    }
+
+    this.getMyLocation = this.getMyLocation.bind(this)
+    this.app = firedatabase.initializeApp(config);
+    this.database = this.app.database().ref().child('favorite_foods');
+  }
+
+
+
+  componentDidMount() {
+    this.getMyLocation()
+  }
+  // got this code from stackoverflow After successfully implementing a react geolocation 
+  // I switched to this method because it works better with other components and it sets state
+  // so it can be passed down as props!
+  getMyLocation() {
+    const location = window.navigator && window.navigator.geolocation
+
+    if (location) {
+      location.getCurrentPosition((position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        })
+      }, (error) => {
+        this.setState({ latitude: 'err-latitude', longitude: 'err-longitude' })
+      })
+    }
+
+  }
+
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Dinner Decider</h1>
       </div>
-    );
+    )
   }
 }
+
 
 export default App;
