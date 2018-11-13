a7189ff8ebd81f2c6d713a9bbcf01e13c7a95037
Juan Baires <juancbaires@gmail.com>
2018-11-13 17:29:29 -0500

    made changes to the login form

--- src/components/User/Login.js
+++ src/components/User/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './User.css'
-
+import { Checkbox, Button, Form, FormControl, FormGroup, Col, ControlLabel } from 'react-bootstrap'
 class Login extends Component {
   state = {
     username: '',
@@ -22,13 +22,39 @@ class Login extends Component {
         <div className="accountIMAGE">
           <div className='image'></div>
         </div>
-        {/* <form onSubmit={this.handleSubmit}>
-          <h3>Login</h3>
-          <p>Please enter your username and password</p>
-          <input onChange={this.handleChange} name="username" placeholder="Username"></input>
-          <input onChange={this.handleChange} type="password" name="password" placeholder="Password"></input>
-          <button>Login</button>
-        </form> */}
+        <div className="sideForm">
+          <Form horizontal>
+            <FormGroup controlId="formHorizontalEmail">
+              <Col componentClass={ControlLabel} sm={2}>
+                Email
+    </Col>
+              <Col sm={10}>
+                <FormControl type="email" placeholder="Email" />
+              </Col>
+            </FormGroup>
+
+            <FormGroup controlId="formHorizontalPassword">
+              <Col componentClass={ControlLabel} sm={2}>
+                Password
+    </Col>
+              <Col sm={10}>
+                <FormControl type="password" placeholder="Password" />
+              </Col>
+            </FormGroup>
+
+            <FormGroup>
+              <Col smOffset={2} sm={10}>
+                <Checkbox>Remember me</Checkbox>
+              </Col>
+            </FormGroup>
+
+            <FormGroup>
+              <Col smOffset={2} sm={10}>
+                <Button type="submit">Sign in</Button>
+              </Col>
+            </FormGroup>
+          </Form>
+        </div>
       </div >
     );
   }
