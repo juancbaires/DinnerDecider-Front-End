277553d9a520fd1717de3cd5bac17ca9ecaeb297
Juan Baires <juancbaires@gmail.com>
2018-11-14 14:22:13 -0500

    made some changes to the signin form

--- src/components/User/Signup.js
+++ src/components/User/Signup.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { FormControl, FormGroup, InputGroup, ControlLabel, Button } from 'react-bootstrap'
 class Signup extends Component {
     state = {
         username: '',
@@ -10,7 +10,9 @@ class Signup extends Component {
         food4: '',
         food5: '',
         food6: '',
+        validationState: ''
     }
+
 
     handleInput = (e) => {
         this.setState({
@@ -19,37 +21,106 @@ class Signup extends Component {
     }
 
     handleSubmit = (e) => {
-      e.preventDefault()
-      this.props.handleSignup(this.state)
+        e.preventDefault()
+        this.props.handleSignup(this.state)
+    }
+
+    checkvalidation = (e) => {
+        if (e.target.value === '') {
+            this.setState({
+                validationState: 'error'
+            })
+        } else {
+            this.setState({
+                validationState: 'success'
+            })
+        }
     }
     render() {
         return (
-
-            <div className="accountForms accountForms--Signup">
-                <h3>Signup</h3>
-                <p>Create username and password</p>
-                <form onSubmit={this.handleSubmit}>
+            <div className="signupContainer">
+                <div className="imageContainer">
+                    <div className="signupImage"></div>
+                </div>
+                <form onSubmit={this.handleSubmit} className="signupForms">
                     <div>
-                        <div className="account--head">
-                            <input onChange={this.handleInput} name="username" placeholder="Username"></input>
-                            <input type="password" onChange={this.handleInput} name="password" placeholder="Password"></input>
-                        </div>
-                        <div className='account-container'>
-                            <div className="account--body">
-                                <p>Spouse one's favorite foods</p>
-                                <input onChange={this.handleInput} name="food1" placeholder="Favorite Food 1"></input>
-                                <input onChange={this.handleInput} name="food2" placeholder="Favorite Food 2"></input>
-                                <input onChange={this.handleInput} name="food3" placeholder="Favorite Food 3"></input>
-                            </div>
-                            <div className="account--body">
-                                <p>Spouse two's favorite foods</p>
-                                <input onChange={this.handleInput} name="food4" placeholder="Favorite Food 1"></input>
-                                <input onChange={this.handleInput} name="food5" placeholder="Favorite Food 2"></input>
-                                <input onChange={this.handleInput} name="food6" placeholder="Favorite Food 3"></input>
-                            </div>
-                        </div>
+                        <h4>Spouse One/ Friend One</h4>
+                        <FormGroup onSubmit={this.handleSubmit} onSelect={this.checkvalidation} validationState={this.state.validationState}>
+                            <ControlLabel>username</ControlLabel>{' '}
+                            <InputGroup>
+                                <InputGroup.Addon>1</InputGroup.Addon>
+                                <FormControl name="username" onChange={this.handleInput} type="text" />
+                            </InputGroup>
+                            <FormControl.Feedback />
+                        </FormGroup>
+                        <FormGroup onSelect={this.checkvalidation} validationState={this.state.validationState}>
+                            <ControlLabel>Food One</ControlLabel>{' '}
+                            <InputGroup>
+                                <InputGroup.Addon>1</InputGroup.Addon>
+                                <FormControl name="food1" onChange={this.handleInput} type="text" />
+                            </InputGroup>
+                            <FormControl.Feedback />
+                        </FormGroup>
+                        <FormGroup onSelect={this.checkvalidation} validationState={this.state.validationState}>
+                            <ControlLabel>Food Two</ControlLabel>{' '}
+                            <InputGroup>
+                                <InputGroup.Addon>2</InputGroup.Addon>
+                                <FormControl name="food2" onChange={this.handleInput} type="text" />
+                            </InputGroup>
+                            <FormControl.Feedback />
+                        </FormGroup>
+                        <FormGroup onSelect={this.checkvalidation} validationState={this.state.validationState}>
+                            <ControlLabel>Food Three</ControlLabel>{' '}
+                            <InputGroup>
+                                <InputGroup.Addon>3</InputGroup.Addon>
+                                <FormControl name="food3" onChange={this.handleInput} type="text" />
+                            </InputGroup>
+                            <FormControl.Feedback />
+                            <Button bsSize="large" block>
+                                Back
+                    </Button>
+                        </FormGroup>
+
                     </div>
-                    <button className="signupButton">Signup</button>
+                    <div>
+                        <h4>Spouse Two/ Friend Two</h4>
+                        <FormGroup onSelect={this.checkvalidation} type="password" validationState={this.state.validationState}>
+                            <ControlLabel>Password</ControlLabel>{' '}
+                            <InputGroup>
+                                <InputGroup.Addon></InputGroup.Addon>
+                                <FormControl type="password" name="password" onChange={this.handleInput} />
+                            </InputGroup>
+                            <FormControl.Feedback />
+                        </FormGroup>
+                        <FormGroup onSelect={this.checkvalidation} validationState={this.state.validationState}>
+                            <ControlLabel>Food One</ControlLabel>{' '}
+                            <InputGroup>
+                                <InputGroup.Addon>1</InputGroup.Addon>
+                                <FormControl name="food4" onChange={this.handleInput} type="text" />
+                            </InputGroup>
+                            <FormControl.Feedback />
+                        </FormGroup>
+                        <FormGroup onSelect={this.checkvalidation} validationState={this.state.validationState}>
+                            <ControlLabel>Food Two</ControlLabel>{' '}
+                            <InputGroup>
+                                <InputGroup.Addon>2</InputGroup.Addon>
+                                <FormControl name="food5" onChange={this.handleInput} type="text" />
+                            </InputGroup>
+                            <FormControl.Feedback />
+                        </FormGroup>
+                        <FormGroup onSelect={this.checkvalidation} validationState={this.state.validationState}>
+                            <ControlLabel>Food Three</ControlLabel>{' '}
+                            <InputGroup>
+                                <InputGroup.Addon>3</InputGroup.Addon>
+                                <FormControl name="food6" onChange={this.handleInput} type="text" />
+                            </InputGroup>
+                            <FormControl.Feedback />
+                            <Button handleSubmit={this.handleSubmit} bsSize="large" block>
+                                Submit
+                    </Button>
+                        </FormGroup>
+
+                    </div>
                 </form>
             </div >
 
