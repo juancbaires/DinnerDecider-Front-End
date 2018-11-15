9da57353d80562ae9387297f55d996039dea6d72
Luis Ortiz <ortizlu@icloud.com>
2018-11-15 13:14:05 -0500

    made the show form look like our sign up form

--- src/components/User/Show.js
+++ src/components/User/Show.js
@@ -9,27 +9,30 @@ class Show extends Component {
         return (
             <div className="show-page">
                 {/* user.username is undefined the first time, so nothing gets rendered. The second time, both are truthy so JS returns the second truthy, which is user.username.toUpperCase()  */}
-                <h1 className="show-title">Hey {user.username && user.username.toUpperCase()}</h1>
-                 <form onSubmit={this.handleSubmit} class="show-form">
-                        <div className='foods'>
-                            <div className="person1-foods">
-                                <p>Person One's favorite foods</p>
-                                <input value={user.food1} onChange={this.handleInput} name="food1" placeholder="Favorite Food 1"></input>
-                                <input value={user.food2} onChange={this.handleInput} name="food2" placeholder="Favorite Food 2"></input>
-                                <input value={user.food3} onChange={this.handleInput} name="food3" placeholder="Favorite Food 3"></input>
+                <h1 className="show-title">{user.username && "Hey " + user.username.charAt(0).toUpperCase() + user.username.slice(1)}</h1>
+                <form onSubmit={this.handleSubmit} className="LoginForm">
+                        <div className="foods">
+                            <div className="foods--list">
+                                <h1>Person 1's foods</h1>
+                                <label htmlFor="">Food 1</label>
+                                <input className="foodInput" name="food1" onChange={this.handleInput} placeholder={user.food1}></input>
+                                <label htmlFor="">Food 2</label>
+                                <input className="foodInput" name="food2" onChange={this.handleInput} placeholder={user.food2}></input>
+                                <label htmlFor="">Food 3</label>
+                                <input className="foodInput" name="food3" onChange={this.handleInput} placeholder={user.food3}></input>
                             </div>
-                            <div className="person2-foods">
-                                <p>Person Two's favorite foods</p>
-                                <input value={user.food4} onChange={this.handleInput} name="food4" placeholder="Favorite Food 1"></input>
-                                <input value={user.food5} onChange={this.handleInput} name="food5" placeholder="Favorite Food 2"></input>
-                                <input value={user.food6} onChange={this.handleInput} name="food6" placeholder="Favorite Food 3"></input>
+                            <div className="foods--list">
+                                <h1>Person 2's foods</h1>
+                                <label htmlFor="">Food 1</label>
+                                <input className="foodInput" name="food4" onChange={this.handleInput} placeholder={user.food4}></input>
+                                <label htmlFor="">Food 2</label>
+                                <input className="foodInput" name="food5" onChange={this.handleInput} placeholder={user.food5}></input>
+                                <label htmlFor="">Food 3</label>
+                                <input className="foodInput" name="food6" onChange={this.handleInput} placeholder={user.food6}></input>
                             </div>
                         </div>
-                    <div className="show-button-container">
-                      <button className="btn">Save</button>
-                      <Link className="random-diner"to="/ateball">Random Diner</Link>
-                    </div>
-                </form>
+                        <button type="submit" className="loginButton">Signup</button>
+                    </form>
             </div>
         );
     }
