2b0ac64cace40ddb4547791ac3c53cd62407c207
Juan Baires <juancbaires@gmail.com>
2018-11-16 14:25:40 -0500

    made changes to yelp request

---
+++ build/precache-manifest.b8495cc2e9de4d86d4937e2272c6660c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e69b920e0ef4e83451ba54cd0f1f75b",
+    "url": "/static/media/ShowimageTwo.7e69b920.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2879c69393ba103568d4",
+    "url": "/static/js/main.2879c693.chunk.js"
+  },
+  {
+    "revision": "b94e48ad824dd9fd3a86",
+    "url": "/static/js/1.b94e48ad.chunk.js"
+  },
+  {
+    "revision": "2879c69393ba103568d4",
+    "url": "/static/css/main.4b7aafa2.chunk.css"
+  },
+  {
+    "revision": "239fb0d228bfda98286fb9af443c1d4f",
+    "url": "/index.html"
+  }
+];
